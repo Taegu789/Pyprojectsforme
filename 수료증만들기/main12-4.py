7ff40d2b4b2e8dde85d932582de08d9ff640fa67
Taegu789 <gvfc789@naver.com>
2022-11-03 07:02:57 +0900

    nㅜㅜㅜㅜ

---
+++ 수료증만들기/main12-4.py
@@ -0,0 +1,102 @@
+from docx import Document
+from openpyxl import load_workbook
+import docx
+from docx.oxml.ns import qn
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx2pdf import convert
+import glob
+load_wb =  load_workbook(r"C:\Users\gvfc7\OneDrive\Desktop\Pyprojectsforme\수료증만들기\레알수료증명단.xlsx")
+load_ws =load_wb.active
+
+name_list = []
+birthday_list = []
+ho_list = []
+for i in range(1,load_ws.max_row + 1):
+    name_list.append(load_ws.cell(i, 1).value)
+    birthday_list.append(load_ws.cell(i, 2).value)
+    ho_list.append(load_ws.cell(i, 3).value)
+    
+print(name_list)
+print(birthday_list)
+print(ho_list)
+
+for i in range(len(name_list)):
+    doc = docx.Document(r"C:\Users\gvfc7\OneDrive\Desktop\Pyprojectsforme\수료증만들기\수료증양식.docx")
+    style = doc.styles['Normal']
+    style.font.name = '나눔고딕'
+    style._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    style.font.size = docx.shared.Pt(12)
+    
+    para = doc.add_paragraph()
+    run = para.add_run('\n\n') 
+    run = para.add_run('              제 '+ ho_list[i] +' 호\n')
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+
+    para = doc.add_paragraph()
+    run = para.add_run('\n\n')
+    run = para.add_run('수  료  증') 
+    run.font.name = '나눔고딕'
+    run.bold = True
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(40)
+    para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    para = doc.add_paragraph()
+    run = para.add_run('\n\n')
+    run = para.add_run('        성       명: ' + name_list[i] +'\n') 
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+    run = para.add_run('        생 년 월 일: ' + birthday_list[i] +'\n') 
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+    run = para.add_run('        교 육 과 정: 파이썬과 40개의 작품들\n')
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20) 
+    run = para.add_run('        교 육 날 짜: 2021.08.05~2021.09.09\n') 
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+
+    para = doc.add_paragraph()
+    run = para.add_run('\n\n')
+    run = para.add_run('        위 사람은 파이썬과 40개의 작품들 교육과정을\n') 
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+    run = para.add_run('        이수하였으므로 이 증서를 수여 합니다.\n') 
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+
+    para = doc.add_paragraph()
+    run = para.add_run('\n\n\n')
+    run = para.add_run('2021.09.19') 
+    run.font.name = '나눔고딕'
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+    para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    para = doc.add_paragraph()
+    run = para.add_run('\n\n\n')
+    run = para.add_run('파이썬교육기관장') 
+    run.font.name = '나눔고딕'
+    run.bold = True
+    run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+    run.font.size = docx.shared.Pt(20)
+    para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    doc.save('수료증만들기\\'+name_list[i]+'.docx')
+    inputs = 'C:\Users\gvfc7\OneDrive\Desktop\Pyprojectsforme\수료증만들기\name_list[i].docx'
+    outputs = 'C:\Users\gvfc7\OneDrive\Desktop\Pyprojectsforme\수료증만들기\name_list[i].pdf'
+    convert(inputs,outputs)
+    #pywintypes.com_error: (-2147221005, '잘못된 클래스 문자열입니다.', None, None)
+    #32비트로 동작하기 때문이다. 그러므로 32비트 파이썬을 설치해준다
+    # 그후에는 vscode 인터프리티어에서 32비트로 설정후 다시 install 해주어야함
+    # convert 부분이 작동안해주어서 그부분 고쳐주었음
+ 
+   
